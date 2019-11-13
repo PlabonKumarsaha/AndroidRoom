d32d2e39ed1d4614e371eb38106321ecfd7a1c1b
PKS <pkumarsaha71@gmail.com>
2019-11-13 13:06:04 +0600

    new

--- app/src/main/java/com/example/androidroom/MainActivity.java
+++ app/src/main/java/com/example/androidroom/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.androidroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     //https://developer.android.com/jetpack/docs/guide
     //Room library - wrapper around sqlite. WIth usual sqlite we have alot of chances to make errors.
@@ -15,9 +22,29 @@ public class MainActivity extends AppCompatActivity {
      i.e. the programmer must write a lot of code to do minimal jobs.*/
     //Room also provides compile time varifications.
 
+    private NoteViewModel noteViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // directly don't create a instance bcz we don't need this new instance every time.so instead we specify
+        //what class model we need.
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        //observe is a live data model.
+        //observer will only update activity when it is in the fork ground.it will automaticaly destroy the meory
+        //so it will avoid memeory leak
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                //method is called each time data is changed!
+                //if we rotate devce this will not hold the activity and change the data
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
